d2a639d4f668d1ca7b8c1797faf4bebe6b98e936
Ximena <ximena.velasco@cceo.com.mx>
2024-03-05 11:08:33 -0600

    feat: :sparkles: Multiple Props

--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -1,14 +1,12 @@
 <script setup>
-defineProps(['title'])
+defineProps(['title', 'id', 'body', 'colorText'])
 </script>
 
 <template>
     <div class="card">
         <div class="card-body">
-            <h5 class="card-title">{{ title }}</h5>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis hic totam asperiores inventore ipsa
-                cupiditate neque vel impedit ducimus ab dolores atque deserunt dolorum quae, autem debitis assumenda omnis
-                excepturi.</p>
+            <h5 class="card-title" :class="`text-${colorText}`">{{ id }} - {{ tittle }}</h5>
+            <p>{{ body }}</p>
         </div>
     </div>
 </template>
