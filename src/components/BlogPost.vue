1b73ad9ba00782a3f6a52231abd16bbb81e3178d
Ximena <ximena.velasco@cceo.com.mx>
2024-03-05 11:26:50 -0600

    feat: :sparkles: Props default value

--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -1,11 +1,13 @@
 <script setup>
-defineProps(['title', 'id', 'body', 'colorText'])
+const props = defineProps(["title", "id", "body", "colorText"])
+console.log(props.title);
+console.log(props.id);
 </script>
 
 <template>
     <div class="card">
         <div class="card-body">
-            <h5 class="card-title" :class="`text-${colorText}`">{{ id }} - {{ tittle }}</h5>
+            <h5 class="card-title" :class="`text-${colorText}`">{{ id }} - {{ title }}</h5>
             <p>{{ body }}</p>
         </div>
     </div>
