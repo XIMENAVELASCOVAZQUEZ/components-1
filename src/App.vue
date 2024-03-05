530dbc37a9702974aafef864d7df8c2359d5aa00
Ximena <ximena.velasco@cceo.com.mx>
2024-03-05 09:45:02 -0600

    feat: :sparkles: Script setup

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,23 @@
-<script setup>
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const counter = ref(0);
+    
+    const increment = () => {
+      counter.value++;
+    };
+
+    return {
+      counter,
+      increment,
+    };
+  },
+};
 </script>
 
 <template>
   <h1>APP</h1>
+  <button @click="increment">{{ counter }}</button>
 </template>
