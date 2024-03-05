6d6f575ae16571bb318d8a4b444ce76855bfbfae
Ximena <ximena.velasco@cceo.com.mx>
2024-03-05 10:34:42 -0600

    feat(bootstrap): :lipstick: NPM Installing

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup>
 import ButtonCounter from './components/ButtonCounter.vue'
 
-
 </script>
 
-
 <template>
-  <h1>APP</h1>
-  <ButtonCounter ></ButtonCounter>
-  <button-counter></button-counter>
+  <div class="container">
+    <h1>APP</h1>
+    <ButtonCounter></ButtonCounter>
+    <button-counter></button-counter>
+  </div>
 </template>
